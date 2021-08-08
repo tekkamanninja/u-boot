243850a1b106c01fc8da7084808a4e888b117edc
Samuel Holland <samuel@sholland.org>
2021-08-08 10:27:44 -0500

    serial: Use the RISC-V SBI interface as a debug console
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ drivers/serial/serial_sbi.c
@@ -0,0 +1,15 @@
+#include <debug_uart.h>
+#include <asm/sbi.h>
+
+static inline void _debug_uart_init(void)
+{
+}
+
+static inline void _debug_uart_putc(int c)
+{
+#if CONFIG_IS_ENABLED(RISCV_SMODE)
+	sbi_console_putchar(c);
+#endif
+}
+
+DEBUG_UART_FUNCS
