c12dd24e2b85aec18f57736f81ad635734ca92c2
Wei Fu <wefu@redhat.com>
2023-05-23 17:50:20 +0800

    config/th1520_lpi4a.h: add more env option for booting linux
    Signed-off-by: Wei Fu <wefu@redhat.com>
    Co-authored-by: Yixun Lan <dlan@gentoo.org>

--- include/configs/th1520_lpi4a.h
+++ include/configs/th1520_lpi4a.h
@@ -17,6 +17,18 @@
 /* Environment options */
 
 #define CFG_EXTRA_ENV_SETTINGS \
+	"kernel_addr_r=0x82000000\0" \
+	"kernel_comp_addr_r=0x88000000\0" \
+	"kernel_comp_size=0x4000000\0" \
+	"ramdisk_addr_r=0x8d300000\0" \
+	"fdt_addr_r=0x8e000000\0" \
+	"fdt_addr_fixed=0x20000000\0" \
+	"scriptaddr=0x8e400000\0" \
+	"scriptfile=uEnv_light_lpi4a.txt\0" \
+	"devtype=mmc\0" \
+	"devnum=0\0" \
+	"bootpart=2\0" \
+	"bootcmd=ext4load mmc ${devnum}:${bootpart} ${scriptaddr} ${scriptfile}; env import -t ${scriptaddr} ${filesize};boot\0" \
 	"PS1=[LPi4A]# \0"
 
 #endif /* __TH1520_LPI4A_H */
