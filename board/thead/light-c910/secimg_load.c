1f887991e8557953feb5dfeceb53f01961dc9cf9
thead_admin <occ_thead@service.alibaba.com>
2023-11-29 01:32:36 +0000

    master
    Signed-off-by: thead_admin <occ_thead@service.alibaba.com>

--- board/thead/light-c910/secimg_load.c
+++ board/thead/light-c910/secimg_load.c
@@ -10,7 +10,7 @@
 #include "sec_library.h"
 
 #define ENV_SECIMG_LOAD     "sec_m_load"
-#define VAL_SECIMG_LOAD     "ext4load mmc 0:7 $tf_addr trust_firmware.bin; ext4load mmc 0:7 $tee_addr tee.bin"
+#define VAL_SECIMG_LOAD     "ext4load mmc ${mmcdev}:${mmcteepart} $tf_addr trust_firmware.bin; ext4load mmc ${mmcdev}:${mmcteepart} $tee_addr tee.bin\0"
 
 #define RPMB_BLOCK_SIZE 256
 #define RPMB_ROLLBACK_BLOCK_START 1
@@ -23,6 +23,7 @@ static const unsigned char emmc_rpmb_key_sample[32] = {0x33, 0x22, 0x11, 0x00, 0
 #endif
 
 extern int sprintf(char *buf, const char *fmt, ...);
+extern char * get_slot_name_suffix(void);
 
 static int get_rpmb_key(uint8_t key[32])
 {
@@ -117,10 +118,18 @@ int verify_and_load_tee_tf_image(void)
    non-secure boot in lifecycle(INIT) */
 bool get_system_boot_type(void)
 {
-	bool btype = false; /* false: non-secure boot | true: secure boot */
+	bool btype = true; /* false: non-secure boot | true: secure boot */
+#if 0
 	int lc = 0;
 	sboot_st_t sb_flag = SECURE_BOOT_DIS;
 	int ret = 0;
+#endif
+	int sb_emulater = 0;
+
+	sb_emulater = env_get_ulong("sb_emulater", 10, 0);
+	if (sb_emulater == 0) {
+		btype = false;
+	}
 # if 0
 	ret = csi_efuse_get_lc(&lc);
 	/* 0: LC_INIT, 1: LC_DEV, 2: LC_OEM, 3: LC_PRO */
@@ -189,18 +198,36 @@ static int do_secimg_load(cmd_tbl_t *cmdtp, int flag, int argc, char * const arg
     bool sb_enable = false;
     const char *secimgs_load_str = VAL_SECIMG_LOAD;
     int ret = -1;
-    sb_enable = get_system_boot_type();
-    if (sb_enable) {
-        /* By default, the value for ENV-SEC-M-LOAD is always to load opensbi image.
-         * if secure boot is enable, we force to change the value to load tee image.
-         * but Never to save it in volatile-RAM
-         */
-        ret = env_set(ENV_SECIMG_LOAD, secimgs_load_str);
-        if (ret != 0) {
-            printf("Rewrite ENV (%s) fails\n", ENV_SECIMG_LOAD);
-            return CMD_RET_FAILURE;
-        }
-    }
+    int teepart = 0;
+
+#ifdef CONFIG_ANDROID_AB
+	char *slot_suffix = get_slot_name_suffix();
+    teepart = env_get_ulong("mmcteepart", 10, 8);
+	if ((strcmp(slot_suffix, "_a") == 0) && (teepart != 8)) {
+		/* Switch mmcbootpart to "_b" */
+		env_set_ulong("mmcbootpart", 2);
+		/* Switch mmcteepart to "_b" */
+		env_set_ulong("mmcteepart", 8);
+	} else if ((strcmp(slot_suffix, "_b") == 0) && (teepart != 9)){
+		/* Switch mmcbootpart to "_b" */
+		env_set_ulong("mmcbootpart", 3);
+		/* Switch mmcteepart to "_b" */
+		env_set_ulong("mmcteepart", 9);
+	}
+#endif
+
+	sb_enable = get_system_boot_type();
+	if (sb_enable) {
+		/* By default, the value for ENV-SEC-M-LOAD is always to load opensbi image.
+		* if secure boot is enable, we force to change the value to load tee image.
+		* but Never to save it in volatile-RAM
+		*/
+		ret = env_set(ENV_SECIMG_LOAD, secimgs_load_str);
+		if (ret != 0) {
+			printf("Rewrite ENV (%s) fails\n", ENV_SECIMG_LOAD);
+			return CMD_RET_FAILURE;
+		}
+	}
 
 	return CMD_RET_SUCCESS;
 }
